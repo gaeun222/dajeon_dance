033efe68ad14221367677d08ad367a0f4b339642
gaeun222 <srehke123@gmail.com>
2025-03-07 16:46:06 +0900

    main_0307_2

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ var swiper = new Swiper(".mySwiper", {
 
 // aos 호출
 AOS.init();
+
+
+// notice 탭
+const tabItem = document.querySelectorAll(".tab__item");
+const tabContent = document.querySelectorAll(".tab__content");
+
+tabItem.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault(); // a 
+    
+    tabContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    tabItem.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    tabItem[index].classList.add("active");
+    tabContent[index].classList.add("active");
+  });
+});
